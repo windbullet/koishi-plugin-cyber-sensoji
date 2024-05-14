20e33c142674edb3ec965323fe6b623d05655bec
windbullet <2536810643@qq.com>
2024-05-14 11:27:20 +0800

    提交文件

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-cyber-sensoji?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-cyber-sensoji)
 
 浅草寺抽签插件
+重写自 https://github.com/Raidenneox/nonebot_plugin_CyberSensoji
+部分修改及优化
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,63 @@
-import { Context, Schema } from 'koishi'
+// This file is modified from https://github.com/Raidenneox/nonebot_plugin_CyberSensoji, under the MIT license
+// Copyright Raidenneox 2022
+
+import { Context, Schema, Random } from 'koishi'
+import { qian } from './qian'
+import { } from 'koishi-plugin-puppeteer'
 
 export const name = 'cyber-sensoji'
 
-export interface Config {}
+export interface Config {
+  imageMode: boolean
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  imageMode: Schema.boolean()
+    .description("以图片形式发送签文（需要puppeteer服务）")
+    .default(false),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const inject = {optional: ["puppeteer"]}
+
+export function apply(ctx: Context, config: Config) {
+  ctx.command("浅草寺抽签", "抽签")
+    .action(async ({session}) => {
+      const num = Random.int(1, 101)
+      if (num >= 5) {
+        if (config.imageMode) {
+          if (ctx.puppeteer) {
+            return ctx.puppeteer.render(
+`<html>
+  <head>
+    <style>
+      html {
+        width: 25%;
+        height: 0;
+      }
+      p {
+        padding: 10px;
+        word-wrap: break-word;
+        white-space: pre-wrap;
+      }
+    </style>
+  </head>
+
+  <body>
+    <p>
+${Random.pick(qian)}
+    </p>
+  </body>
+</html>`
+)
+          } else {
+            ctx.logger('cyber-sensoji').warn("未启用puppeteer服务，无法使用图片模式")
+            return Random.pick(qian)
+          }
+        } else {
+          return Random.pick(qian)
+        }
+      } else {
+        return "是空签呢"
+      }
+    })
 }
